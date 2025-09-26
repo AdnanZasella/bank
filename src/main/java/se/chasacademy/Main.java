db9c531da84b4c42468c58e9bec97bcc317975bb
AdnanZasella <adnanzasella@hotmail.com>
2025-09-26 10:17:36 +0200

    Meny system med val av 1. insättning 2. uttag 3. visa saldo 4. av slut där transaktion logiken i transaktion klassen

--- src/main/java/se/chasacademy/Main.java
+++ src/main/java/se/chasacademy/Main.java
@@ -1,17 +1,62 @@
 package se.chasacademy;
-
+import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner sc = new Scanner(System.in);
+        double saldo = 0;
+
+
+
+
+
+
+        while (true) {
+
+            System.out.println("Välkommen till Swedbank, välj en av alternativen");
+            System.out.println("1. Insättning");
+            System.out.println("2. Uttag ");
+            System.out.println("3. Se saldo ");
+            System.out.println("4. Avsluta ");
+
+            int val = sc.nextInt();
+
+
+            if (val == 1) {
+
+                System.out.println("Hur mycket pengar vill du sätta in i ditt konto?");
+
+                double amount = sc.nextDouble();
+
+                Transaction transaction = new Transaction(amount);
+
+                saldo += transaction.getAmount();
+
+                System.out.println(transaction);
+
+            } else if (val == 2) {
+
+                System.out.println("Hur mycket pengar vill du dra ut? ");
+
+                double amount = sc.nextDouble();
+
+                Transaction transaction = new Transaction(- amount);
+
+                saldo += transaction.getAmount();
+
+                System.out.println(transaction);
+            }
+
+            else if (val == 3) {
+
+                System.out.println("💰 Ditt saldo är: " + saldo + " kr");
+
+            }
+
+
         }
+
     }
-}
+    }
--- src/main/java/se/chasacademy/Transaction.java
+++ src/main/java/se/chasacademy/Transaction.java
@@ -1,4 +1,28 @@
 package se.chasacademy;
 
 public class Transaction {
+
+    private double amount;
+    private double saldo;
+
+
+    public Transaction(double amount) {
+        this.amount = amount;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    @Override
+    public String toString() {
+
+        if (amount >= 0) {
+            return "Insättning: " + amount + " kr";
+        } else {
+            return "Uttag: " + (-amount) + " kr"; // ta bort minustecknet
+        }
+
+    }
+
 }
